900e47a1cdc8a8f3c8de81ebf10f83d337027eb2
Anton Babushking <napishi_antonu@mail.ru>
2020-11-25 20:03:29 +0300

    догоняю

--- src/test/java/tests/AlfaBankTests.java
+++ src/test/java/tests/AlfaBankTests.java
@@ -1,7 +1,10 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AlfaBankTests {
@@ -9,6 +12,8 @@ public class AlfaBankTests {
     @Test
     void TestAlfaBank(){
         open("https://alfabank.ru/");
-        $("span").sibling(1).closest("а").find("Вклады").click();
+        $("a[href='/cards/'").closest("span").parent().sibling(0).find("a").click();
+        $(byText("Что такое вклад?")).parent().preceding(0).find("span").click();
+        $("html").shouldHave(Condition.text("Федеральный закон от 23.12.2003 N 177-ФЗ"));
     }
 }
